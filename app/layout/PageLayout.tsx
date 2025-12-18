e0fe8e5dd0ead6a45241731f1a639e75244b06ba
Raymond Chan <raymondch49@gmail.com>
2025-12-18 16:57:51 -0500

    refractor: double spacing fixed

--- app/layout/PageLayout.tsx
+++ app/layout/PageLayout.tsx
@@ -5,7 +5,7 @@ export default function PageLayout({
 }) {
   return (
     <main className="w-full md:max-w-[500px] mx-auto px-4 py-12">
-      <article className="prose prose-zinc dark:prose-invert max-w-none">
+      <article>
         {children}
       </article>
     </main>
